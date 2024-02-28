c4001c3d800e30891d697cfdaf7f75709aafbc30
ParoguaCode <parogua.code@gmail.com>
2024-02-28 18:07:54 -0400

    comentario

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Registrar nuevo producto') }}
+            {{ __('Registrar nuevo producto Guadalupe') }}
         </h2>
     </x-slot>
 
