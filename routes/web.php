0c55180b56071f412cc63ee217457d7ee09a6f69
BenjaminRo-Dev <programador.ben@gmail.com>
2024-03-02 17:00:56 -0400

    Eliminado controlador y vistas productos

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::resource('productos', ProductoController::class)
-    ->middleware(['auth'])
-    ->except(['show']);
+// Route::resource('productos', ProductoController::class)
+//     ->middleware(['auth'])
+//     ->except(['show']);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
